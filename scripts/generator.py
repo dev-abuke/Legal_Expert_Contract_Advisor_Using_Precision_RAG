c26e1409820eac0458dc6c20eb654ae88328c8aa
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-05 07:41:36 +0000

    feat: scropts

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,7 +1,7 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 def create_rag_chain(retriever, prompt, llm):
     return (
@@ -11,7 +11,7 @@ def create_rag_chain(retriever, prompt, llm):
         | StrOutputParser()
     )
 
-def create_prompt():
+def create_context_prompt():
 
     # Prompt
     template = """Answer the question based only on the following context:
@@ -23,6 +23,40 @@ def create_prompt():
     prompt = ChatPromptTemplate.from_template(template)
 
     return prompt
+
+def create_history_aware_prompt():
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    return contextualize_q_prompt
+
+def get_qa_assistant_prompt():
+    ### Answer question ###
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Use three sentences maximum and keep the answer concise.\
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    return qa_prompt
 
 def create_llm(temperature=0):
     # LLM
--- scripts/retrieval.py
+++ scripts/retrieval.py
@@ -1,11 +1,35 @@
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+import chromadb
+from chromadb.config import Settings
+from chromadb.api.models import Document
+from chromadb.api.types import QueryResult
+from langchain.embeddings.openai import OpenAIEmbeddings
+from chromadb.api import Chroma
 
-def get_retrieval_embeddings(splits):
-    vectorstore = Chroma.from_documents(documents=splits, 
-                                    embedding=OpenAIEmbeddings())
+class Retriever:
+    def __init__(self, documents: list):
+        self.documents = documents
+        self.client = chromadb.Client(Settings(chroma_store_path="chroma_store"))
+        self.collection = self.client.get_or_create_collection(name="documents")
 
-    retriever = vectorstore.as_retriever()
+        # Embed and store documents
+        self.embeddings = OpenAIEmbeddings()
+        self.store_documents(documents)
 
-    return retriever
+    def store_documents(self, documents: list):
+        for doc in documents:
+            embedding = self.embeddings.embed(doc['text'])
+            self.collection.add(doc['id'], embedding, doc)
 
+    def retrieve(self, query: str) -> QueryResult:
+        query_embedding = self.embeddings.embed(query)
+        results = self.collection.query(query_embedding)
+        return results
+
+# # Singleton instance of the Retriever
+# retriever_instance = None
+
+# def get_retriever_instance(documents: list = None):
+#     global retriever_instance
+#     if retriever_instance is None:
+#         retriever_instance = Retriever(documents)
+#     return retriever_instance
