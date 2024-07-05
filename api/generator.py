51d438bfac502c21c9a37d15f5c0508aaf6b30f7
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-05 14:49:53 +0000

    feat: update generator for query translations

--- api/generator.py
+++ api/generator.py
@@ -1,15 +1,13 @@
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from operator import itemgetter
 
-def create_rag_chain(retriever, prompt, llm):
-    return (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
+from .retriever import get_retriever_instance
+from .factory import get_model
+from utils.helpers import get_unique_union
+
+retriever = get_retriever_instance()
 
 def create_context_prompt():
 
@@ -58,8 +56,61 @@ def get_qa_assistant_prompt():
 
     return qa_prompt
 
+def multi_query_prompt():
+    # Multi Query: Different Perspectives
+    template = """You are an AI language model assistant. Your task is to generate five 
+    different versions of the given user question to retrieve relevant documents from a vector 
+    database. By generating multiple perspectives on the user question, your goal is to help
+    the user overcome some of the limitations of the distance-based similarity search. 
+    Provide these alternative questions separated by newlines. Original question: {question}"""
+    prompt_perspectives = ChatPromptTemplate.from_template(template)
+
+    return prompt_perspectives
+
 def create_llm(temperature=0):
     # LLM
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=temperature)
 
     return llm
+
+def get_answer_using_multi_query(question):
+    prompt_perspectives = multi_query_prompt()
+
+    generate_queries = (
+        prompt_perspectives 
+        | ChatOpenAI(temperature=0) 
+        | StrOutputParser() 
+        | (lambda x: x.split("\n"))
+    )
+
+    retrieval_chain = generate_queries | retriever.get_retriever().map() | get_unique_union
+
+    docs = retrieval_chain.invoke({"question":question})
+
+    context = "\n\n".join(doc.page_content for doc in docs)
+
+    prompt = create_context_prompt()
+
+    llm = get_model()
+
+    final_rag_chain = (
+        {"context": retrieval_chain, 
+        "question": itemgetter("question")} 
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    answer = final_rag_chain.invoke({"question":question})
+
+    return answer, context
+
+# TODO: Implement
+def get_answer_using_rag_fusion(question):
+    return "This is using RAG Fusion", question
+
+def get_answer_using_decomposition(question):
+    return "This is using Decomposition", question
+
+def get_answer_using_hyde(question):
+    return "This is using HyDE", question
