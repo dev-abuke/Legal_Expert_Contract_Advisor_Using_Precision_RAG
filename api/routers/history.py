840bfc8cdb9cd02ae9bb5ba0f361771419f15129
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-05 14:51:56 +0000

    feat: ability to get the unique session id in the api

--- api/routers/history.py
+++ api/routers/history.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import Session
 from ..schemas import MessageHistoryResponse
 from ..database import get_db
 from ..models import ChatHistory
+from sqlalchemy import distinct
 from ..services.history_service import get_chat_history
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -13,11 +18,18 @@ async def history_endpoint(session_id: str, db: Session = Depends(get_db)):
     history = get_chat_history(session_id, db)
     return history
 
-
 @router.get("/history/", response_model=List[MessageHistoryResponse])
 async def get_all_history(db: Session = Depends(get_db)):
     history = db.query(ChatHistory).all()
     return history
+
+#get all unique session ids
+@router.get("/session_ids", response_model=List[str])
+async def get_session_ids(db: Session = Depends(get_db)):
+    # get all unique session ids as a list
+    unique_session_ids = db.query(distinct(ChatHistory.session_id)).all()
+    logger.info(f"The Unique Session IDs :: {unique_session_ids}")
+    return [session_id[0] for session_id in unique_session_ids]
 
 @router.delete("/history/{session_id}")
 async def delete_history(session_id: str, db: Session = Depends(get_db)):
