fc89a75a68369c92c2a12e7458c35281059856c0
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-08 01:24:12 +0000

    refactor: retriever class to add differnt retriever trypes

--- api/retriever.py
+++ api/retriever.py
@@ -5,45 +5,48 @@ from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from .factory import get_text_splitter
+
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
+
+from .factory import get_text_splitter, get_retriever
+from .config import load_config
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Embed and store the texts
+config = load_config()
 
 class Retriever:
     def __init__(self, documents: list):
         self.documents = documents
-        self.persist_directory = 'db'
-        # Embed and store documents
-        self.embeddings = OpenAIEmbeddings()
-        if documents is not None:
-            logger.info(f"Storing documents {len(documents)}")
-            # self.store_documents(documents)
-            self.db = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(), persist_directory=self.persist_directory)
-        self.db = Chroma(persist_directory=self.persist_directory, embedding_function=OpenAIEmbeddings())
-            
 
+        self.persist_directory = 'db'
+
+        self.retriever_type = config["retriever"]
+
+        self.retriever = get_retriever()
+            
     def store_documents(self, documents: list[Document]):
         logger.info(f"Storing documents From function {len(documents)}")
 
-        # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = get_text_splitter().split_documents(documents)
 
         logger.info(f"Splits {len(splits)}")
 
-        docs = self.db.add_documents(splits)
+        docs = self.retriever.add_documents(splits)
         
         logger.info(f"Documents added {docs[0]}")
 
     def retrieve(self, query: str):
-        results = self.db.similarity_search(query)
+        results = self.retriever.similarity_search(query)
         return results
     def get_retriever(self):
-        return self.db.as_retriever()
+        if config["retriever"] == "dense":
+            return self.retriever.as_retriever()
+        elif config["retriever"] == "hybrid":
+            return self.retriever
 
 # Singleton instance of the Retriever
 retriever_instance = None
