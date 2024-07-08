ebc37e0f47bef43214a680e8010d23a5937e40b5
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-08 01:25:19 +0000

    refactor: unused imports

--- api/services/qa_service.py
+++ api/services/qa_service.py
@@ -1,31 +1,30 @@
-from fastapi import HTTPException
+# Imports for memory Q&A
+# import bs4
+# from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# from langchain_core.runnables.history import RunnableWithMessageHistory
+
 from sqlalchemy.orm import Session
+
 from ..models import ChatHistory
 from ..schemas import QueryRequest
-from ..factory import get_model, get_query_translation
-from ..retriever import get_retriever_instance
+from ..factory import get_query_translation
+
 import logging
 
-import bs4
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-llm = get_model()
 query_translation = get_query_translation()
-retriever = get_retriever_instance()
 
 async def get_answer(session_id: str, query: QueryRequest, db: Session):
 
     # answer, context = get_answer_using_multi_query(query.query)
-    answer, context = query_translation(query)
+    answer, context = query_translation(query.query)
 
     print(f"Context is joined :: {context}")
 
