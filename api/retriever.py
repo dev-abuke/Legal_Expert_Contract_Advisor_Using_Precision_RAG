9e235777a4eeb649d916c719af73154c05fba717
Abubeker Shamil <abubekershamil123@gmail.com>
2024-07-09 10:01:42 +0000

    feat: add weviate hybrid retriever

--- api/retriever.py
+++ api/retriever.py
@@ -1,17 +1,15 @@
-import chromadb
-from chromadb.config import Settings
-from chromadb.api.types import QueryResult
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_chroma import Chroma
 from langchain_core.documents import Document
-
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
+from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
 
-from .factory import get_text_splitter, get_retriever
+from .factory import get_text_splitter
 from .config import load_config
 
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -26,7 +24,33 @@ class Retriever:
 
         self.retriever_type = config["retriever"]
 
-        self.retriever = get_retriever()
+        if config["retriever"] == "dense":
+            self.retriever = Chroma(persist_directory=self.persist_directory, embedding_function=OpenAIEmbeddings())
+
+        elif config["retriever"] == "hybrid":
+            print("We are Using Weaviate Hybrid Search Retriever")
+            import weaviate
+
+            auth_config = weaviate.auth.AuthApiKey(api_key=os.getenv("WEAVIATE_API_KEY"))
+
+            client = weaviate.Client(
+                url="https://sandbox-rag-hrim3oyf.weaviate.network",
+                additional_headers={
+                        "X-Openai-Api-Key": os.getenv("OPENAI_API_KEY"),
+                },
+                auth_client_secret=auth_config
+            )
+
+            self.retriever = WeaviateHybridSearchRetriever(
+                client=client,
+                index_name="LangChain",
+                text_key="text",
+                attributes=[],
+                create_schema_if_missing=True,
+            )
+            print("Loaded the Hybrid Search Retriever", self.retriever)
+        else:
+            raise ValueError("Invalid retriever type")
             
     def store_documents(self, documents: list[Document]):
         logger.info(f"Storing documents From function {len(documents)}")
@@ -43,9 +67,11 @@ class Retriever:
         results = self.retriever.similarity_search(query)
         return results
     def get_retriever(self):
+        print("getting the retriever===================", self.retriever)
         if config["retriever"] == "dense":
             return self.retriever.as_retriever()
         elif config["retriever"] == "hybrid":
+            print("++++++++++++++++++++======Inside Get Retriever If condition =======+++++++++++++") 
             return self.retriever
 
 # Singleton instance of the Retriever
